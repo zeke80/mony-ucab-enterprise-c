45f3f1bc2f65f986696fe6de156679035697a17e
Roberto Carbajales <robertocd98@gmail.com>
2020-06-01 14:06:15 -0400

    comodando rutas de tabs e incorporando pantalla de login

--- Post_virtual/src/app/tabs/tabs-routing.module.ts
+++ Post_virtual/src/app/tabs/tabs-routing.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TabsPage } from './tabs.page';
+import { LoginPage } from '../login/login.page';
 
 const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginPage,
+    loadChildren: () => import('../login/login.module').then(m => m.LoginPageModule)
+  },
   {
     path: 'tabs',
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
+        path: 'perfil',
         loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
       },
       {
-        path: 'tab2',
+        path: 'cuenta',
         loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
       },
       {
-        path: 'tab3',
+        path: 'operaciones',
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
       {
@@ -28,7 +34,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/login',
     pathMatch: 'full'
   }
 ];
