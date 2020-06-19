5effd63a53c357aea935f2b98b92b59e16cd9bfa
candypops <mariagabriela236@gmail.com>
2020-06-19 02:16:02 -0400

    anñadidas rutas de login, sign-up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './home/nav-menu/nav-menu.component';
+import { LoginFormComponent } from './home/login-form/login-form.component';
+import { SignupFormComponent } from './home/signup-form/signup-form.component';
+import { HomeComponent } from './home/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent
+    NavMenuComponent,
+    LoginFormComponent,
+    SignupFormComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([
+      
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'sign-up', component: SignupFormComponent },
+      { path: 'login', component: LoginFormComponent }
+      
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
