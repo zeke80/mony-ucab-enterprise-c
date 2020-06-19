4d1d3aa9ac4b54e60b3578623ce1292ebd8d0fb6
candypops <mariagabriela236@gmail.com>
2020-06-19 03:04:47 -0400

    añadida tabla movimientos

---
+++ src/app/authorised/tabla-movimientos/tabla-movimientos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla-movimientos',
+  templateUrl: './tabla-movimientos.component.html',
+  styleUrls: ['./tabla-movimientos.component.css']
+})
+export class TablaMovimientosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
