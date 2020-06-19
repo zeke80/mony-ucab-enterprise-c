68252d2eef35bcc86fb3c13d4ce1b24bceaf8ea9
candypops <mariagabriela236@gmail.com>
2020-06-19 03:04:06 -0400

    añadido componente bloquar operaciones

---
+++ src/app/authorised/bloquear/bloquear.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bloquear',
+  templateUrl: './bloquear.component.html',
+  styleUrls: ['./bloquear.component.css']
+})
+export class BloquearComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
