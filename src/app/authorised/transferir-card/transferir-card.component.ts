d87c714a3facf69656ee59380365da85ecc987e6
candypops <mariagabriela236@gmail.com>
2020-06-19 03:05:58 -0400

    creado base para diseño de las transferencias

---
+++ src/app/authorised/transferir-card/transferir-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-transferir-card',
+  templateUrl: './transferir-card.component.html',
+  styleUrls: ['./transferir-card.component.css']
+})
+export class TransferirCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
