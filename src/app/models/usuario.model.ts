5b5324dc60efd3ed0b964a0c720ebbd6ffe00ac8
candypops <mariagabriela236@gmail.com>
2020-06-21 04:25:04 -0400

    agregado modelo usuario

---
+++ src/app/models/usuario.model.ts
@@ -0,0 +1,18 @@
+
+export class Usuario {
+    constructor(
+        public idUsuario : number,
+        public idTipoUsuario : number,
+        public idTipoIdentificacion : number,
+        public usuario : string,
+        public fechaRegistro : string,
+        public nroIdentificacion : number,
+        public email : string,
+        public telefono : string,
+        public direccion : string,
+        public estatus : number
+    ){
+
+    }
+    
+}
