8cd1a7cc27a9aaef404e893773b26c786753bbea
candypops <mariagabriela236@gmail.com>
2020-06-21 21:28:26 -0400

    ahora muestra todas las transacciones del usuario. falta filtrar por tiempo

--- src/app/authorised/tabla-movimientos/tabla-movimientos.component.ts
+++ src/app/authorised/tabla-movimientos/tabla-movimientos.component.ts
@@ -1,3 +1,4 @@
+import { MovimientosService } from './services/movimientos.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TablaMovimientosComponent implements OnInit {
 
-  constructor() { }
+  movimientosCuenta : any;
+  movimientosTarjeta : any;
+  movimientosMonedero : any;
+
+  constructor(public s_movimientos : MovimientosService) { }
 
   ngOnInit(): void {
+    this.s_movimientos.consultarCuentas().subscribe(data =>{
+      this.movimientosCuenta = data;
+    });
+
+    this.s_movimientos.consultarTarjeta().subscribe(data =>{
+      this.movimientosTarjeta = data;
+    });
+
+    this.s_movimientos.consutarMonedero() .subscribe(data =>{
+      this.movimientosMonedero = data;
+    });
+    
   }
 
 }
