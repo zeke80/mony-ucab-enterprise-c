548551ba5f99f638fa6f1f9c3097f10464e25470
candypops <mariagabriela236@gmail.com>
2020-06-21 21:27:39 -0400

    agregado servicio para peticiones relacionada con las transacciones

---
+++ src/app/authorised/tabla-movimientos/services/movimientos.service.ts
@@ -0,0 +1,50 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovimientosService {
+
+  constructor( private http : HttpClient) { }
+
+  consultarCuentas(){
+    let url = "http://monyucab.somee.com/api/Usuario/operacionesCuentas";
+
+    let id = parseInt(localStorage.getItem('idUsuario'), 10);
+
+    this.http.post(url, {'id' : id}).
+    toPromise().then((data : any) =>{
+    }
+    )
+    
+    return this.http.post(url, {'id' : id});
+  }
+
+  consultarTarjeta (){
+    let url = "http://monyucab.somee.com/api/Usuario/operacionesTarjetas";
+    
+    let id = parseInt(localStorage.getItem('idUsuario'), 10);
+
+    this.http.post(url, {'id' : id}).
+    toPromise().then((data : any) =>{
+    }
+    )
+
+    return this.http.post(url, {'id' : id});
+  }
+
+  consutarMonedero (){
+    let url = "http://monyucab.somee.com/api/Usuario/operacionesMonedero"
+
+    let id = parseInt(localStorage.getItem('idUsuario'), 10);
+
+    this.http.post(url, {'id' : id}).
+    toPromise().then((data : any) =>{
+    }
+    )
+
+    return this.http.post(url, {'id' : id});
+  }
+
+}
