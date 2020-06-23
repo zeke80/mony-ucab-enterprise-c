c6ca13e31fce1c7e5585b92ca218015b32cd1d85
candypops <mariagabriela236@gmail.com>
2020-06-22 21:43:12 -0400

    ya funciona filtrar segun los radio buttons. Falta filtrar segun parameto de tiempo

--- src/app/authorised/tabla-movimientos/tabla-movimientos.component.ts
+++ src/app/authorised/tabla-movimientos/tabla-movimientos.component.ts
@@ -12,10 +12,34 @@ export class TablaMovimientosComponent implements OnInit {
   movimientosTarjeta : any;
   movimientosMonedero : any;
   saldo : any;
+  mes1 : number;
+  mes2 : string;
+  anio : number;
+  opcion : string;
 
   constructor(public s_movimientos : MovimientosService) { }
 
   ngOnInit(): void {
+    this.consultarTodo();
+  }
+
+  filtrar(){
+    if (this.opcion == "todos"){
+      this.consultarTodo();
+    }
+    else if (this.opcion == "tarjeta"){
+      this.consultarSoloTarjeta();
+    }
+    else if (this.opcion == "monedero"){
+      this.consultarSoloMonedero();
+
+    }
+    else if (this.opcion == "cuenta"){
+      this.consultarSoloCuenta();
+    }
+  }
+
+  consultarTodo(){
     this.s_movimientos.consultarCuentas().subscribe(data =>{
       this.movimientosCuenta = data;
     });
@@ -33,7 +57,6 @@ export class TablaMovimientosComponent implements OnInit {
     });
     
   }
-
   consultarSoloTarjeta(){
 
     this.s_movimientos.consultarTarjeta().subscribe(data =>{
@@ -62,7 +85,7 @@ export class TablaMovimientosComponent implements OnInit {
     this.s_movimientos.consultarCuentas().subscribe(data =>{
       this.movimientosCuenta = data;
     });
-    
+
     this.movimientosCuenta = null;
 
     this.movimientosTarjeta = null;
