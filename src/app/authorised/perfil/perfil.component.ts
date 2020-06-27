4e03ec51782c575772508338e27767eb4e15c61a
candypops <mariagabriela236@gmail.com>
2020-06-27 02:11:32 -0400

    adaptado al uso de modelo usuario, modelo persona y modelo comercio

--- src/app/authorised/perfil/perfil.component.ts
+++ src/app/authorised/perfil/perfil.component.ts
@@ -1,6 +1,11 @@
-import { PerfilService } from './services/perfil.service';
+
 import { Component, OnInit } from '@angular/core';
 
+import { Usuario } from './../../models/usuario.model';
+import { Persona } from './../../models/persona.model';
+import { Comercio } from './../../models/comercio.model';
+
+import { PerfilService } from './services/perfil.service';
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
@@ -8,9 +13,95 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
+  infoPersona : Persona ={
+    estadoCivil : 0,
+    nombre : '',
+    apellido : '',
+    fechaNacimiento : ''
+  };
+
+  infoComercio : Comercio ={
+    razonSocial : '',
+    nombreRepresentante : '',
+    apellidoRepresentante : ''
+  };
+
+  infoUsuario : Usuario = {
+    idUsuario : 0,
+    idTipoUsuario : 0,
+    idTipoIdentificacion : 0,
+    usuario : '',
+    fechaRegistro : '',
+    nroIdentificacion : 0,
+    email : '',
+    telefono : '',
+    direccion : '',
+    estatus : 0
+  };
+  estado : number;
+  estadoCivil : number;
+  tipoIdentificacion : number;
+  tipoId : number;
+  isDisabled : boolean = true;
+
   constructor(public s_perfil : PerfilService) { }
 
   ngOnInit(): void {
+    this.consultar();
   }
 
+  consultar(){
+    this.consutarUsuario();
+    if (parseInt(localStorage.getItem('idTipoUsuario'), 10) == 1){
+      this.consutarPersona();
+    }
+    else if (parseInt(localStorage.getItem('idTipoUsuario'), 10) == 2){
+      this.consultarComercio();
+    }
+  }
+  consutarPersona(){
+    this.s_perfil.consultarPersona().subscribe((data : any) => {
+      this.infoPersona.estadoCivil = data.idestadocivil;
+      this.infoPersona.nombre = data.nombre;
+      this.infoPersona.apellido = data.apellido;
+      this.infoPersona.fechaNacimiento = data.fecha_nacimiento;
+    }
+    );
+    this.infoComercio = null;
+  }
+
+  consultarComercio(){
+    this.s_perfil.consultarComercio().subscribe((data : any) =>{
+      this.infoComercio.razonSocial = data.razon_social;
+      this.infoComercio.nombreRepresentante = data.nombre_representante;
+      this.infoComercio.apellidoRepresentante = data.apellido_representante;
+
+    });
+
+    this.infoPersona = null;
+
+  }
+
+  consutarUsuario(){
+    this.s_perfil.consultarUSuario().subscribe((data : any) =>{
+      this.infoUsuario.idUsuario = data.idusuario;
+      this.infoUsuario.idTipoUsuario = data.idtipousuario;
+      this.infoUsuario.idTipoIdentificacion = data.idtipoidentificacion;
+      this.infoUsuario.usuario = data.usuario;
+      this.infoUsuario.fechaRegistro = data.fecha_registro;
+      this.infoUsuario.nroIdentificacion = data.nro_identificacion;
+      this.infoUsuario.email = data.email;
+      this.infoUsuario.telefono = data.telefono;
+      this.infoUsuario.direccion = data.direccion;
+      this.infoUsuario.estatus = data.estatus;
+    });
+  }
+
+  editar(){
+    this.isDisabled = false;
+  }
+
+  cancelar(){
+    this.isDisabled = true;
+  }
 }
--- src/app/authorised/perfil/services/perfil.service.ts
+++ src/app/authorised/perfil/services/perfil.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -7,5 +8,61 @@ export class PerfilService {
 
   show = false;
   
-  constructor() { }
+  constructor(private http : HttpClient) {
+  }
+
+  consultarUSuario(){
+    let url = "http://monyucab.somee.com/api/Usuario/infoUsuario";
+
+    let id = parseInt(localStorage.getItem('idUsuario'), 10);
+
+    this.http.post(url, {'id' : id}).
+    toPromise().then((data : any) =>{
+    }
+    )
+    
+    return this.http.post(url, {'id' : id});
+
+  }
+
+  consultarPersona(){
+    let url = "http://monyucab.somee.com/api/Usuario/infoPersona";
+
+    let id = parseInt(localStorage.getItem('idUsuario'), 10);
+
+    this.http.post(url, {'id' : id}).
+    toPromise().then((data : any) =>{
+    }
+    )
+    
+    return this.http.post(url, {'id' : id});
+
+  }
+
+  consultarComercio(){
+    let url = "http://monyucab.somee.com/api/Usuario/infoComercio";
+
+    let id = parseInt(localStorage.getItem('idUsuario'), 10);
+
+
+    this.http.post(url, {'id' : id}).
+    toPromise().then((data : any) =>{
+    }
+    )
+    
+    return this.http.post(url, {'id' : id});
+
+  }
+
+  ajustarPersona(){
+
+  }
+
+  ajustarComercio(){
+
+  }
+
+  ajustarUsuario(){
+
+  }
 }
