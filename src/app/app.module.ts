f6d14d9ef80eaeae3d70f5fbca03472f1352479a
candypops <mariagabriela236@gmail.com>
2020-06-29 03:13:08 -0400

    agregada funcion recuperar contrasena con validaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
@@ -59,6 +59,7 @@ import { RecuperarContrasenaComponent } from './home/recuperar-contrasena/recupe
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       
       { path: '', component: HomeComponent, pathMatch: 'full' },
---
+++ src/app/home/recuperar-contrasena/recuperar-contrasena.component.ts
@@ -0,0 +1,36 @@
+
+import { Component, OnInit } from '@angular/core';
+
+import { RecuperarContrasenaService } from './services/recuperar-contrasena.service';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-recuperar-contrasena',
+  templateUrl: './recuperar-contrasena.component.html',
+  styleUrls: ['./recuperar-contrasena.component.css']
+})
+export class RecuperarContrasenaComponent implements OnInit {
+
+  recuperarContrasenaFormulario = new FormGroup({
+    correo : new FormControl('', Validators.email)
+  })
+  email = '';
+
+  constructor(private s_contrasena : RecuperarContrasenaService) { }
+
+  ngOnInit(): void {
+  }
+
+  recuperarContrasena(email : string){
+    console.log(email);
+    this.s_contrasena.recuperContrasena(email.toLocaleUpperCase())
+    .subscribe((data: any) =>{
+      alert('Correo enviado');
+    } );
+  }
+
+  get correo (): AbstractControl{
+    return this.recuperarContrasenaFormulario.get('correo'); 
+  }
+}
+
